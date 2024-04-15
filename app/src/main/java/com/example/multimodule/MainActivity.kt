2a0d1db774601c2edc064da4a654a0a1d8489e62
stanislavrepinskas <repinskas@gmail.com>
2024-04-15 11:38:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/multimodule/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.multimodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController: NavController = findNavController(R.id.navHostFragment)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigation.setupWithNavController(navController)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MultiModule"
+include(":app")
+include("feature:cart")
+include("feature:cart-api")
+include("feature:product")
+include("feature:product-api")
+include("feature:catalog")
+include("feature:profile")
+include(":core")
